fdd8c5483716b5deba933af818f3bef0dcf7f3a7
Ouda Wycliffe <ouda.wycliffe@gmail.com>
2023-08-31 14:24:48 +0300

    initial next prisma db server set up

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,22 @@
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+const SetupPage = async () => {
+    const profile = await initialProfile();
+    const server = await db.server.findFirst({
+        where: {
+            member: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    })
+
+    if(server){
+        return redirect(`/servers/${server.id}`)
+    }
+    return <div>Create a Server</div>;
+}
+ 
+export default SetupPage;
